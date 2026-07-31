b152fbe33f29d35a8d9522cab68438003bafea9c
colin969 <colinb969@gmail.com>
2026-07-31 19:31:13 +0100

    Allow insecure origins for apx

--- apx/server.go
+++ apx/server.go
@@ -180,7 +180,8 @@ func (h apxHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func (s apxServer) serveConn(w http.ResponseWriter, r *http.Request, reduced bool) {
 	c, err := websocket.Accept(w, r, &websocket.AcceptOptions{
 		// Adds about 32mb memory usage per 1k connections, debatble CPU usage
-		CompressionMode: websocket.CompressionContextTakeover,
+		CompressionMode:    websocket.CompressionContextTakeover,
+		InsecureSkipVerify: true,
 	})
 	if err != nil {
 		s.logf("%v", err)
